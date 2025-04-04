806a11c1bfcddbcd5625f26bd0d5b7e04dc93432
KondkariYusuf <kondkariyusuf345@gmail.com>
2025-04-04 21:19:58 +0530

    Template Literals

--- templateLiterals.js
+++ templateLiterals.js
@@ -1,5 +1,6 @@
-templateLiteral(){
-    var user = document.getElementById("name").innerHTML ;
+function templateLiteral(){
+    var user = document.getElementById("name").value ;
 
-    document.getElementById("templateLiteral").innerHTML = `Hello ${user}, welcome to the world of JavaScript!`;
+    document.getElementById("templateLiterals").innerHTML = `<h1>Hello ${user}, welcome to the world of JavaScript!</h1>`;
 }
+console.log("hello world")
